9bd5168bfb66242e72990f865afef27e561a3359
Jonathan <30914983+s49676537@users.noreply.github.com>
2018-01-02 23:44:34 +1300

    Visual Update
    changed backgrounds
    landing page pretty cool now

--- game.js
+++ game.js
@@ -411,8 +411,10 @@ function playAudio(id){
 }
 
 function toggle(id) {
-	if (document.getElementById(id).style.display != "none") {document.getElementById(id).style.display = "none";}
-	else {document.getElementById(id).style.display = "block";}
+	if (document.getElementById(id).style.display === "none"){
+		document.getElementById(id).style.display = "block";
+	}
+	else {document.getElementById(id).style.display = "none";}
 }
 
 main();
